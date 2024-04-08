a585719b18f64c0eed6ba9cbe7e48171064bf7b1
Shakes <jappopiano@gmail.com>
2024-04-08 23:18:51 +0200

    Support characters in mtl file team ID replacing
    Also makes sure to look for model/character/uniform/common specifically
    for the /XXX/ replacing

--- Engines/python/lib/mtl_id_change.py
+++ Engines/python/lib/mtl_id_change.py
@@ -11,8 +11,8 @@ def mtl_id_change(mtl_path, team_id = "000"):
         mtl_path (str): The path to the .mtl file.
         team_id (str, optional): The team ID to replace in the file. Defaults to "000".
 
-    This function looks for /XXX/ or for u0XXXp inside the path of the samplers,
-    where XXX is any sequence of three digits, and replaces XXX with the team ID provided as parameter.
+    This function looks for model/character/uniform/common/XXX/ or for u0XXXp inside the path of the samplers,
+    where XXX is any sequence of three characters, and replaces XXX with the team ID provided as parameter.
     """
 
     # Try to unzlib the file
@@ -31,8 +31,8 @@ def mtl_id_change(mtl_path, team_id = "000"):
     for sampler in root.findall('.//sampler'):
         path = sampler.get('path')
         if path is not None:
-            path = re.sub(r'/(\d){3}/', '/'+team_id+'/', path)
-            path = re.sub(r'u0(\d){3}p', 'u0'+team_id+'p', path)
+            path = re.sub(r'model/character/uniform/common/([a-zA-Z0-9]){3}/', 'model/character/uniform/common/'+team_id+'/', path)
+            path = re.sub(r'u0([a-zA-Z0-9]){3}p', 'u0'+team_id+'p', path)
             sampler.set('path', path)
 
     tree.write(mtl_path)

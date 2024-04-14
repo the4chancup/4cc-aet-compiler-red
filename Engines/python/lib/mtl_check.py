6345e381bc888474e2a1e3e454d50f13c59437f8
Shakes <jappopiano@gmail.com>
2024-04-15 00:41:25 +0200

    Overhaul mtl checking module
    Unified info structure in the messages
    Specific info for bad xml format error
    Checks for repeated materials
    Missing state names check reintroduced, is now a warning
    Checks if the textures listed exist in the proper folders
    Writes the log environment var

--- Engines/python/lib/mtl_check.py
+++ Engines/python/lib/mtl_check.py
@@ -13,12 +13,13 @@ def mtl_check(mtl_path):
     Returns:
         bool: False if the .mtl file is valid, True otherwise.
     """
-
-    # Store the name of the parent folder
-    mtl_folder_name = os.path.basename(os.path.dirname(mtl_path))
     
-    # Store the name of the mtl and its parent folder
-    mtl_name = os.path.join(mtl_folder_name, os.path.basename(mtl_path))
+    # Read the necessary parameters
+    pause_on_error = int(os.environ.get('PAUSE_ON_ERROR', '1'))
+    
+    # Store the name of the file and its parent folder
+    mtl_name = os.path.basename(mtl_path)
+    mtl_folder_name = os.path.basename(os.path.dirname(mtl_path))
     
     # Try to unzlib the file
     unzlib_file(mtl_path)
@@ -26,9 +27,21 @@ def mtl_check(mtl_path):
     # Parse the file
     try:
         tree = ET.parse(mtl_path)
-    except:
-        print(f"- ERROR: {mtl_name} is not a valid .mtl file")
-        input('Press Enter to continue...')
+    except ET.ParseError as e:
+        import xml.parsers.expat
+        error_string = xml.parsers.expat.ErrorString(e.code)
+        line, column = e.position
+        print("- ERROR - Invalid .mtl file")
+        print(f"- Folder:      {mtl_folder_name}")
+        print(f"- MTL name:    {mtl_name}")
+        print(f"- Issue:       \"{error_string}\"")
+        print(f"- Location:    At or before line {line}, column {column}")
+        print("-")
+        
+        if pause_on_error:
+            input('Press Enter to continue...')
+            
+        os.environ["LOG"] = "1"
         return True
     
     root = tree.getroot()
@@ -45,9 +58,29 @@ def mtl_check(mtl_path):
     ]
 
     error = False
+    warning = False
+    
+    previous_material_name_list = []
     
     for material in root.findall('material'):
         
+        error_conflict = False
+        
+        # Check if the name of the material is in the list of previous material names
+        if material.get('name') in previous_material_name_list:
+            print(f"- ERROR - The material \"{material.get('name')}\" in {mtl_name}")
+            print("- is listed more than once")
+            print("-")
+            
+            error_conflict = True
+        
+        # Add the name of the material to the list
+        previous_material_name_list.append(material.get('name'))
+        
+        if error_conflict:
+            error = True
+            continue
+
         # Create dictionary of state names and values
         state_name_dict = {}
         for state in material.findall('state'):
@@ -57,22 +90,28 @@ def mtl_check(mtl_path):
         
             # Check that the value of ztest is 1
             if name == 'ztest' and value != '1':
-                print(f"- ERROR: ztest must be 1 in the material \"{material.get('name')}\"")
-                print(f"- in {mtl_name}")
+                print("- ERROR - Value of state \"ztest\" must be 1")
+                print(f"- MTL name:    {mtl_name}")
+                print(f"- Material:    \"{material.get('name')}\"")
+                print(f"- State value: {value}")
                 print("-")
                 error = True
             
             # Check that the value of blendmode is 0
             if name == 'blendmode' and value != '0':
-                print(f"- ERROR: blendmode must be 0 in the material \"{material.get('name')}\"")
-                print(f"- in {mtl_name}")
+                print("- ERROR - Value of state \"blendmode\" must be 0")
+                print(f"- MTL name:    {mtl_name}")
+                print(f"- Material:    \"{material.get('name')}\"")
+                print(f"- State value: {value}")
                 print("-")
                 error = True
             
             # Check that the value of alphablend is 0 or 1
             if name == 'alphablend' and value not in ['0', '1']:
-                print(f"- ERROR: alphablend must be 0 or 1 in the material \"{material.get('name')}\"")
-                print(f"- in {mtl_name}")
+                print("- ERROR - Value of state \"alphablend\" must be 0 or 1")
+                print(f"- MTL name:    {mtl_name}")
+                print(f"- Material:    \"{material.get('name')}\"")
+                print(f"- State value: {value}")
                 print("-")
                 error = True
             
@@ -80,33 +119,115 @@ def mtl_check(mtl_path):
         missing_state_names = [state_name for state_name in REQUIRED_STATE_NAMES if state_name not in state_name_dict]
         
         if missing_state_names:
-            ##TODO: Remove once the templates have been updated
-            continue
-            print("- ERROR: The following required state names are missing")
-            print(f"- from the material \"{material.get('name')}\" in {mtl_name}:")
+            ##TODO: Convert to error once the templates have been updated
+            ##print("- ERROR - Missing state names")
+            print("- Warning - Missing state names")
+            print(f"- Folder:      {mtl_folder_name}")
+            print(f"- MTL name:    {mtl_name}")
+            print(f"- Material:    \"{material.get('name')}\"")
             # Print the list of missing required state names
-            print("- " + "\n- ".join(missing_state_names))
+            for missing_state_name in missing_state_names:
+                print(f"- State name:  \"{missing_state_name}\"")
             print("-")
-            error = True
+            ##error = True
+            warning = True
         
         else:
+            warning_nonrecvals = False
+            
             # Check that if the value of alphablend is 1, then the value of alphatest is 0
             if state_name_dict['alphablend'] == '1' and not (state_name_dict['zwrite'] == '0'):
-                print(f"- Warning: Non-recommended values for the material \"{material.get('name')}\"")
-                print(f"- in {mtl_name}:")
-                print(f"- alphablend: {state_name_dict['alphablend']} (recommended 1 if semitransparency is needed, 0 otherwise)")
-                print(f"- zwrite:     {state_name_dict['zwrite']} (recommended 0)")
-                print("-")
+                alphablend_recommended_string = 'recommended 1 if semitransparency is needed, 0 otherwise'
+                zwrite_recommended_string = 'recommended 0'
+                
+                warning_nonrecvals = True
                 
             # Check that if the value of alphablend is 0, then the value of zwrite is 1
             if state_name_dict['alphablend'] == '0' and not (state_name_dict['zwrite'] == '1'):
-                print(f"- Warning: Non-recommended values for the material \"{material.get('name')}\"")
-                print(f"- in {mtl_name}:")
-                print(f"- alphablend: {state_name_dict['alphablend']} (recommended 0 if semitransparency is not needed, 1 otherwise)")
-                print(f"- zwrite:     {state_name_dict['zwrite']} (recommended 1)")
+                alphablend_recommended_string = 'recommended 0 if semitransparency is not needed, 1 otherwise'
+                zwrite_recommended_string = 'recommended 1'
+                
+                warning_nonrecvals = True
+                
+            if warning_nonrecvals:
+                print("- Warning - Non-recommended values for states \"alphablend\" and \"zwrite\"")
+                print(f"- Folder:      {mtl_folder_name}")
+                print(f"- MTL name:    {mtl_name}")
+                print(f"- Material:    \"{material.get('name')}\"")
+                print(f"- alphablend:  {state_name_dict['alphablend']} ({alphablend_recommended_string})")
+                print(f"- zwrite:      {state_name_dict['zwrite']} ({zwrite_recommended_string})")
                 print("-")
+                warning = True
+        
+        # Check, for each sampler, that the texture path corresponds to a texture file in the folder indicated
+        for sampler in material.findall('sampler'):
+            sampler_texture_path = sampler.get('path')
+            
+            if not sampler_texture_path:
+                print("- ERROR - Missing texture path")
+                print(f"- Folder:      {mtl_folder_name}")
+                print(f"- MTL name:    {mtl_name}")
+                print(f"- Material:    \"{material.get('name')}\"")
+                print(f"- Sampler:     \"{sampler.get('name')}\"")
+                print("-")
+                error = True
+            else:
+                error_texture_missing = False
+                
+                # Check if the texture path is a relative path and the file exists in the path indicated
+                if sampler_texture_path.startswith('./'):
+                    # Remove the "./" from the path
+                    texture_subpath = sampler_texture_path[2:]
+                    texture_path = os.path.join(os.path.dirname(mtl_path), texture_subpath)
+                    
+                    if not os.path.exists(texture_path):
+                        # Remove "extracted_exports/" from the path
+                        texture_path_short = texture_path[18:]
+                        
+                        error_texture_missing = True
+                
+                # Check if the texture path points to the uniform common folder and the file exists in the Common folder of the export
+                elif sampler_texture_path.startswith('model/character/uniform/common/'):
+                    # Remove the "model/character/uniform/common/XXX/" from the path
+                    texture_subpath = sampler_texture_path[35:]
+                    common_folder_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(mtl_path))), "Common")
+                    texture_path = os.path.join(common_folder_path, texture_subpath)
+                    
+                    if not os.path.exists(texture_path):
+                        # Remove "extracted_exports/" from the path
+                        texture_path_short = texture_path[18:]
+                        
+                        error_texture_missing = True
+                
+                # Check if the texture path points to the face common folder
+                elif sampler_texture_path.startswith('model/character/face/common/'):
+                    pass
+                
+                # If the texture path is not a relative path nor points to the common folders, it is unusable
+                else:
+                    texture_path_short = "Unknown"
+                    
+                    error_texture_missing = True
+                    
+                if error_texture_missing:
+                    ##TODO: Convert to error once the templates have been updated
+                    ##print("- ERROR - Texture file does not exist in the path indicated")
+                    print("- Warning - Texture file does not exist in the path indicated")
+                    print(f"- Folder:      {mtl_folder_name}")
+                    print(f"- MTL name:    {mtl_name}")
+                    print(f"- Material:    \"{material.get('name')}\"")
+                    print(f"- Sampler:     \"{sampler.get('name')}\"")
+                    print(f"- Tex path:    {sampler_texture_path}")
+                    print(f"- Full path:   {texture_path_short}")
+                    print("-")
+                    ##error = True
+                    warning = True
+                
     
-    if error:
+    if error and pause_on_error:
         input('Press Enter to continue...')
+    
+    if error or warning:
+        os.environ["LOG"] = "1"
 
     return error

a7e6f17b56ab63749c41c4ef7f58542e2dae89c3
Shakes <jappopiano@gmail.com>
2024-04-16 23:53:34 +0200

    Clean teamid_get

--- Engines/python/lib/teamid_get.py
+++ Engines/python/lib/teamid_get.py
@@ -3,17 +3,17 @@ import shutil
 
 
 # Function for finding the team ID after receiving the foldername as parameter
-def teamid_get(exportfolder_path, team_name, team_id_min, team_id_max):
+def teamid_get(exportfolder_path, team_name_folder, team_id_min, team_id_max):
 
     # Read the necessary parameters
     pause_on_error = int(os.environ.get('PAUSE_ON_ERROR', '1'))
 
     # Prepare a clean version of the team name without slashes
-    team_name_clean = team_name.replace("/", "").replace("\\", "").upper()
+    team_name_folder_clean = team_name_folder.replace("/", "").replace("\\", "").upper()
 
     root_found = None
     not_root = None
-    teamid = None
+    team_id = None
 
     if any([
         os.path.exists(f"{exportfolder_path}/*.txt"),
@@ -28,7 +28,7 @@ def teamid_get(exportfolder_path, team_name, team_id_min, team_id_max):
         os.path.exists(f"{exportfolder_path}/Common"),
         os.path.exists(f"{exportfolder_path}/Other")
     ]):
-        root_found = 1
+        root_found = True
 
 
     # If the folders aren't at the root
@@ -49,20 +49,21 @@ def teamid_get(exportfolder_path, team_name, team_id_min, team_id_max):
                 os.path.exists(f"{exportfolder_path}/{foldername_test}/Common"),
                 os.path.exists(f"{exportfolder_path}/{foldername_test}/Other")
             ]):
-                root_found = 1
-                not_root = 1
+                root_found = True
+                not_root = True
                 foldername_inside = foldername_test
+                break
 
-    # If there's no files anywhere
+    # If there's no usable files anywhere
     if not root_found:
 
         with open('memelist.txt', 'a') as file:
             file.write("-\n")
-            file.write("- {}'s manager needs to get memed on (no files) - Export discarded.\n".format(team_name))
+            file.write(f"- {team_name_folder}'s manager needs to get memed on (no files) - Export discarded.\n")
         memelist = 1
 
         print("-\n")
-        print("- {}'s manager needs to get memed on (no files).".format(team_name))
+        print(f"- {team_name_folder}'s manager needs to get memed on (no files).")
         print("- This export will be discarded.\n")
         print("- Closing the script's window and fixing the export is recommended.\n")
 
@@ -75,9 +76,6 @@ def teamid_get(exportfolder_path, team_name, team_id_min, team_id_max):
         return None
 
 
-    # Reset the flag for usable team name found
-    team_name_good = None
-
     # If the folders are not at the root
     if not_root:
 
@@ -89,51 +87,50 @@ def teamid_get(exportfolder_path, team_name, team_id_min, team_id_max):
         shutil.rmtree(f"{exportfolder_path}/{foldername_inside}")
 
     # Look for a txt file with Note in the filename
-    note_found = None
+    note_name = None
     for file in os.listdir(exportfolder_path):
         if file.endswith(".txt") and "note" in file.lower():
-            note_found = True
-            note_name = f"{team_name_clean} Note.txt"
+            note_name = f"{team_name_folder_clean} Note.txt"
             os.rename(f"{exportfolder_path}/{file}", f"{exportfolder_path}/{note_name}")
+            break
+
+    team_id = None
 
     # If there's a Note file try to get the team name from it
-    if note_found:
+    if note_name:
 
-        team_name_found = None
+        team_name = None
         with open(f"{exportfolder_path}/{note_name}", 'r', encoding="utf8") as file:
             for line in file:
-                if not team_name_found:
-                    if "Team:" in line:
-                        team_name_found = True
-                        team_name = line.split(":", 1)[1].strip()
+                if "Team:" in line:
+                    team_name = line.split(":", 1)[1].strip()
+                    break
 
-        if team_name_found:
+        if team_name:
 
             # If the name on the note file is different than the one on the export foldername print it
-            if team_name.lower() != team_name.lower():
-                print(f"- Actual name: {team_name}")
+            if team_name.lower() != team_name_folder.lower():
+                print(f"- Actual name: {team_name} ", end='')
 
             # Search for the team ID on the list of team names
             with open("./teams_list.txt", 'r') as team_file:
                 for line in team_file.readlines()[1:]:
-                    if not team_name_good:
-                        if team_name.lower() == line.split()[1].lower():
-                            teamid = line.split()[0]
-                            team_name_good = True
+                    if team_name.lower() == line.split()[1].lower():
+                        team_id = line.split()[0]
+                        break
 
     # If there's no Note file or no usable team name was found on it
-    if not team_name_good:
+    if not team_id:
 
         # Check if the team name taken from the export foldername with brackets added is on the list
         with open("./teams_list.txt", 'r') as team_file:
             for line in team_file.readlines()[1:]:
-                if not team_name_good:
-                    if team_name.lower() == line.split()[1].lower():
-                        teamid = line.split()[0]
-                        team_name_good = True
+                if team_name_folder.lower() == line.split()[1].lower():
+                    team_id = line.split()[0]
+                    break
 
     # If no usable team name was found even then
-    if not team_name_good:
+    if not team_id:
 
         with open("memelist.txt", 'a') as memelist:
             memelist.write(f"\n- {team_name}'s manager needs to get memed on (unusable team name) - Export discarded.")
@@ -152,10 +149,10 @@ def teamid_get(exportfolder_path, team_name, team_id_min, team_id_max):
 
         return None
 
-    print(f"(ID: {teamid})")
+    print(f"(ID: {team_id})")
 
     # Check if the team ID is in the range
-    if not (teamid.isdigit() and int(teamid) in range(team_id_min, team_id_max + 1)):
+    if not (team_id.isdigit() and int(team_id) in range(team_id_min, team_id_max + 1)):
 
         with open("memelist.txt", 'a') as memelist:
             memelist.write(f"\n- {team_name}'s manager needs to get memed on (team id not in range) - Export discarded.")
@@ -168,4 +165,4 @@ def teamid_get(exportfolder_path, team_name, team_id_min, team_id_max):
 
         return None
 
-    return teamid
+    return team_id

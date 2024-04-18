75c632cff8eda61c10c3635821d50794a431b82a
Shakes <jappopiano@gmail.com>
2024-04-18 21:24:28 +0200

    Improve broken xml file message

--- Engines/python/lib/export_check.py
+++ Engines/python/lib/export_check.py
@@ -169,8 +169,8 @@ def faces_check(exportfolder_path, team_name):
                             log.write('- (no face.fpk.xml file inside) - Folder discarded)\n')
                             print('- (no face.fpk.xml file inside) - Folder discarded')
                         if folder_error_xml_format:
-                            log.write('- (broken xml files) - Folder discarded)\n')
-                            print('- (broken xml files) - Folder discarded')
+                            log.write('- (broken xml file) - Folder discarded)\n')
+                            print('- (broken xml file) - Folder discarded')
                         if folder_error_edithairxml:
                             log.write('- (unsupported edithair face folder, needs updating) - Folder discarded)\n')
                             print('- (unsupported edithair face folder, needs updating) - Folder discarded')

69aacf08112d1e346b8a025b6021110fb8c0d04e
Shakes <jappopiano@gmail.com>
2024-04-20 15:03:39 +0200

    Improve dependency_check
    Uses importlib.util.find_spec instead of importlib.import_module
    to check for missing libraries, which avoids the need to throw
    exceptions

--- Engines/python/dependency_check.py
+++ Engines/python/dependency_check.py
@@ -1,11 +1,11 @@
 import sys
 import runpy
-import importlib
 import importlib.util
 
 
 # Check if the dependencies are installed
 def dependency_check_on_import():
+
     # Prepare a list of dependencies
     dependencies = [
         "py7zr",
@@ -14,13 +14,17 @@ def dependency_check_on_import():
     if sys.platform == "win32":
         dependencies.append("requests")
 
-    try:
-        for dependency in dependencies:
-            importlib.import_module(dependency)
-    except ImportError:
+    dependencies_missing = []
+
+    for dependency in dependencies:
+        if importlib.util.find_spec(dependency) is None:
+            # Add any missing dependencies to the list
+            dependencies_missing.append(dependency)
+
+    if dependencies_missing:
+
         print("- The following new dependencies were not found:")
         # List the missing dependencies, one per line
-        dependencies_missing = [f"{dependency}" for dependency in dependencies if not importlib.util.find_spec(dependency)]
         for dependency in dependencies_missing:
             print(f"- \"{dependency}\"")
         print("-")

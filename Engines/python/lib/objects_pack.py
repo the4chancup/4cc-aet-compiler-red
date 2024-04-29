655c21126708cf3a42946da80f8a000b9ee464e9
Shakes4cc <jappopiano@gmail.com>
2024-04-29 23:01:13 +0200

    Move generic.fpkd to template folder

--- Engines/python/lib/objects_pack.py
+++ Engines/python/lib/objects_pack.py
@@ -112,8 +112,7 @@ def objects_packer(object_type, object_source_folder, object_destination_folder,
                 os.remove(fpkxml_path)
 
             # Rename the folder for packing
-            os.rename(os.path.join(temp_path, object_id),
-                      os.path.join(temp_path, f"{object_type}_fpk"))
+            os.rename(os.path.join(temp_path, object_id), os.path.join(temp_path, f"{object_type}_fpk"))
 
             # Pack the fpk
             fpk_destination = os.path.join(temp_path, f"{object_type}.fpk")
@@ -136,10 +135,9 @@ def objects_packer(object_type, object_source_folder, object_destination_folder,
             shutil.move(fpk_destination, final_folder_path)
 
             # Copy the generic fpkd to the same folder and rename it
-            generic_fpkd_path = os.path.join("Engines", "generic.fpkd")
+            generic_fpkd_path = os.path.join("Engines", "template", "generic.fpkd")
             shutil.copy(generic_fpkd_path, final_folder_path)
-            os.rename(os.path.join(final_folder_path, "generic.fpkd"),
-                      os.path.join(final_folder_path, f"{object_type}.fpkd"))
+            os.rename(os.path.join(final_folder_path, "generic.fpkd"), os.path.join(final_folder_path, f"{object_type}.fpkd"))
 
             # Move the textures
             if object_type == "face":

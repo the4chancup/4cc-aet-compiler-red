e5bb4697d469cc4fa0547af260eafbdd8e142477
Shakes4cc <jappopiano@gmail.com>
2024-05-11 23:50:55 +0200

    Flatten fmdl_id_change

--- Engines/python/lib/fmdl_id_change.py
+++ Engines/python/lib/fmdl_id_change.py
@@ -55,134 +55,136 @@ def fmdl_id_change(fn, id, teamid = "000"):
 
     #Check if all required blocks were found.
     ##NOTE: Change this when a new block is added
-    if(bcv == 2):
-        #We have all the required offsets now, start building output
-        b.seek(head+stroff)
-
-        #Store string lengths
-        lens = []
-        for i in range(strc):
-            b.seek(2, 1)
-            lens.append(struct.unpack('<H', b.read(2))[0])
-            b.seek(4, 1)
-
-        #Then process MTL
-        strs = [] #Store strings in a list
-        b.seek(sec1off + strend)
-        for i in range(strc):
-            ln = b.read(lens[i]+1).rstrip(b'\0').decode('utf-8')
-            if(ln):
-                strs.append(ln)
-
-        #Scan textures
-        b.seek(head + texoff)
-        paths = [] #Store texture paths
-        for i in range(texc):
-            texp = struct.unpack("<H", b.read(2))[0] #Texture path
-            if texp not in paths:
-                paths.append(texp)
-
-        #Close file for now, we're done reading
-        b.close()
-
-        #Texture Path Count, for error reporting
-        tpc = 0
-        #Process the paths
-        for path in paths:
-            spath = strs[path-1].split("/") #Split path to individual pieces
-
-            #Make sure our magic piece is the player ID and the path is the correct path
-            #Faces path
-            if(len(spath) == 10 and re.fullmatch("[0-9]{5}", spath[7])):
-                if(id.isdigit() and len(id) == 5):
-                    logging.debug("[MTLTools] Successfully changed ID for path " + strs[path-1])
-                    spath[7] = str(id) #Change the ID
-                    npath = "/".join(spath) #Combine the path
-                    strs[path-1] = npath #Overwrite old path in string list
-                    tpc = tpc + 1
-                else:
-                    logging.debug("[MTLTools] Incorrect player ID " + id + ", please make sure the ID is exactly 5 digits long")
-
-            #Refs path
-            if(len(spath) == 10 and re.fullmatch("referee[0-9]{3}", spath[7])):
-                if(re.fullmatch("referee[0-9]{3}", id)):
-                    logging.debug("[MTLTools] Successfully changed ID for path " + strs[path-1])
-                    spath[7] = str(id) #Change the ID
-                    npath = "/".join(spath) #Combine the path
-                    strs[path-1] = npath #Overwrite old path in string list
-                    tpc = tpc + 1
-                else:
-                    logging.debug("[MTLTools] Incorrect referee name " + id)
-
-            #Boots path
-            elif(len(spath) == 8 and re.fullmatch("k[0-9]{4}", spath[6])):
-                if(re.fullmatch("k[0-9]{4}", id)):
-                    logging.debug("[MTLTools] Successfully changed ID for path " + strs[path-1])
-                    spath[6] = id #Change the ID
-                    npath = "/".join(spath) #Combine the path
-                    strs[path-1] = npath #Overwrite old path in string list
-                    tpc = tpc + 1
-                else:
-                    logging.debug("[MTLTools] Incorrect boots ID " + id + ", please make sure the ID is exactly 5 characters long and follows the 'kXXXX' format")
-
-            #Gloves path
-            elif(len(spath) == 8 and re.fullmatch("g[0-9]{4}", spath[6])):
-                if(re.fullmatch("g[0-9]{4}", id)):
-                    logging.debug("[MTLTools] Successfully changed ID for path " + strs[path-1])
-                    spath[6] = id #Change the ID
-                    npath = "/".join(spath) #Combine the path
-                    strs[path-1] = npath #Overwrite old path in string list
-                    tpc = tpc + 1
-                else:
-                    logging.debug("[MTLTools] Incorrect gloves ID " + id + ", please make sure the ID is exactly 5 characters long and follows the 'gXXXX' format")
-
-            #Balls path
-            elif(len(spath) == 7 and re.fullmatch("ball[0-9]{3}", spath[5])):
-                if(re.fullmatch("ball[0-9]{3}", id)):
-                    logging.debug("[MTLTools] Successfully changed ID for path " + strs[path-1])
-                    spath[5] = id #Change the ID
-                    npath = "/".join(spath) #Combine the path
-                    strs[path-1] = npath #Overwrite old path in string list
-                    tpc = tpc + 1
-                else:
-                    logging.debug("[MTLTools] Incorrect ball ID " + id + ", please make sure the ID is exactly 7 characters long and follows the 'ballXXX' format")
-
-            #Common path
-            elif(len(spath) == 9 and re.fullmatch("[0-9]{3}", spath[6])):
-                if(re.fullmatch("[0-9]{3}", teamid)):
-                    logging.debug("[MTLTools] Successfully changed ID for path " + strs[path-1])
-                    spath[6] = teamid #Change the ID
-                    npath = "/".join(spath) #Combine the path
-                    strs[path-1] = npath #Overwrite old path in string list
-                    tpc = tpc + 1
-                else:
-                    logging.debug("[MTLTools] Incorrect Team ID " + teamid + ", please make sure the ID is exactly 3 characters long and follows the 'XXX' format")
-
-            else:
-                logging.debug("[MTLTools] No ID found in path " + strs[path-1])
-
-        #Yell if there were no paths with IDs at all
-        if(tpc == 0):
-            logging.debug("[MTLTools] No paths with IDs found in file " + fn)
-            print("[MTLTools] No paths with IDs found in file " + fn)
-
-        #Re-open file for writing
-        b = open(fn, "r+b")
-
-        #Seek to string block
-        b.seek(sec1off + strend)
-
-        #Re-write FMDL strings
-        for s in strs:
-            b.write(struct.pack("B", 0))
-            b.write(bytes(s, 'utf-8'))
-
-        #Done, close file for good and exit
-        b.close()
-
-    else:
+    if not (bcv == 2):
         logging.debug("1") #Error code 1 - Didn't find required blocks in file (texture definition, string block)
         b.close()
+        return
+
+    #We have all the required offsets now, start building output
+    b.seek(head+stroff)
+
+    #Store string lengths
+    lens = []
+    for i in range(strc):
+        b.seek(2, 1)
+        lens.append(struct.unpack('<H', b.read(2))[0])
+        b.seek(4, 1)
+
+    #Then process MTL
+    strs = [] #Store strings in a list
+    b.seek(sec1off + strend)
+    for i in range(strc):
+        ln = b.read(lens[i]+1).rstrip(b'\0').decode('utf-8')
+        if(ln):
+            strs.append(ln)
+
+    #Scan textures
+    b.seek(head + texoff)
+    paths = [] #Store texture paths
+    for i in range(texc):
+        texp = struct.unpack("<H", b.read(2))[0] #Texture path
+        if texp not in paths:
+            paths.append(texp)
+
+    #Close file for now, we're done reading
+    b.close()
+
+    #Texture Path Count, for error reporting
+    tpc = 0
+    #Process the paths
+    for path in paths:
+        spath = strs[path-1].split("/") #Split path to individual pieces
+
+        #Make sure our magic piece is the player ID and the path is the correct path
+        #Faces path
+        if(len(spath) == 10 and re.fullmatch("[0-9]{5}", spath[7])):
+            if(id.isdigit() and len(id) == 5):
+                logging.debug("[MTLTools] Successfully changed ID for path " + strs[path-1])
+                spath[7] = str(id) #Change the ID
+                npath = "/".join(spath) #Combine the path
+                strs[path-1] = npath #Overwrite old path in string list
+                tpc = tpc + 1
+            else:
+                logging.debug("[MTLTools] Incorrect player ID " + id + ", please make sure the ID is exactly 5 digits long")
+
+        #Refs path
+        if(len(spath) == 10 and re.fullmatch("referee[0-9]{3}", spath[7])):
+            if(re.fullmatch("referee[0-9]{3}", id)):
+                logging.debug("[MTLTools] Successfully changed ID for path " + strs[path-1])
+                spath[7] = str(id) #Change the ID
+                npath = "/".join(spath) #Combine the path
+                strs[path-1] = npath #Overwrite old path in string list
+                tpc = tpc + 1
+            else:
+                logging.debug("[MTLTools] Incorrect referee name " + id)
+
+        #Boots path
+        elif(len(spath) == 8 and re.fullmatch("k[0-9]{4}", spath[6])):
+            if(re.fullmatch("k[0-9]{4}", id)):
+                logging.debug("[MTLTools] Successfully changed ID for path " + strs[path-1])
+                spath[6] = id #Change the ID
+                npath = "/".join(spath) #Combine the path
+                strs[path-1] = npath #Overwrite old path in string list
+                tpc = tpc + 1
+            else:
+                logging.debug("[MTLTools] Incorrect boots ID " + id + ", please make sure the ID is exactly 5 characters long and follows the 'kXXXX' format")
+
+        #Gloves path
+        elif(len(spath) == 8 and re.fullmatch("g[0-9]{4}", spath[6])):
+            if(re.fullmatch("g[0-9]{4}", id)):
+                logging.debug("[MTLTools] Successfully changed ID for path " + strs[path-1])
+                spath[6] = id #Change the ID
+                npath = "/".join(spath) #Combine the path
+                strs[path-1] = npath #Overwrite old path in string list
+                tpc = tpc + 1
+            else:
+                logging.debug("[MTLTools] Incorrect gloves ID " + id + ", please make sure the ID is exactly 5 characters long and follows the 'gXXXX' format")
+
+        #Balls path
+        elif(len(spath) == 7 and re.fullmatch("ball[0-9]{3}", spath[5])):
+            if(re.fullmatch("ball[0-9]{3}", id)):
+                logging.debug("[MTLTools] Successfully changed ID for path " + strs[path-1])
+                spath[5] = id #Change the ID
+                npath = "/".join(spath) #Combine the path
+                strs[path-1] = npath #Overwrite old path in string list
+                tpc = tpc + 1
+            else:
+                logging.debug("[MTLTools] Incorrect ball ID " + id + ", please make sure the ID is exactly 7 characters long and follows the 'ballXXX' format")
+
+        #Common path
+        elif(len(spath) == 9 and re.fullmatch("[0-9]{3}", spath[6])):
+            if(re.fullmatch("[0-9]{3}", teamid)):
+                logging.debug("[MTLTools] Successfully changed ID for path " + strs[path-1])
+                spath[6] = teamid #Change the ID
+                npath = "/".join(spath) #Combine the path
+                strs[path-1] = npath #Overwrite old path in string list
+                tpc = tpc + 1
+            else:
+                logging.debug("[MTLTools] Incorrect Team ID " + teamid + ", please make sure the ID is exactly 3 characters long and follows the 'XXX' format")
+
+        else:
+            logging.debug("[MTLTools] No ID found in path " + strs[path-1])
+
+    #Yell if there were no paths with IDs at all
+    if(tpc == 0):
+        logging.debug("[MTLTools] No paths with IDs found in file " + fn)
+        print("[MTLTools] No paths with IDs found in file " + fn)
+
+    #Re-open file for writing
+    b = open(fn, "r+b")
+
+    #Seek to string block
+    b.seek(sec1off + strend)
+
+    #Re-write FMDL strings
+    for s in strs:
+        b.write(struct.pack("B", 0))
+        b.write(bytes(s, 'utf-8'))
+
+    #Done, close file for good and exit
+    b.close()
+
+    return
 
 def main():
     if not((len(sys.argv) >= 3) and (len(sys.argv) <= 4)):

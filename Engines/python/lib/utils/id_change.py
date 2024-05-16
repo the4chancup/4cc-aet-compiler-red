ee1a2081c35f748b3af1a67e168046d8d436df5f
Shakes4cc <jappopiano@gmail.com>
2024-05-16 23:15:09 +0200

    Genericise common paths on id_change

--- Engines/python/lib/utils/id_change.py
+++ Engines/python/lib/utils/id_change.py
@@ -9,11 +9,11 @@ def path_id_change(path, team_id):
         path (str): The path string.
         team_id (str): The team ID to replace in the file.
 
-    This function looks for model/character/uniform/common/XXX/, u0XXXp and u0XXXg inside the path string,
+    This function looks for common/XXX/, u0XXXp and u0XXXg inside the path string,
     where XXX is any sequence of three characters, and replaces XXX with the team ID provided as parameter.
     """
 
-    path = re.sub(r'model/character/uniform/common/([a-zA-Z0-9]){3}/', 'model/character/uniform/common/'+team_id+'/', path)
+    path = re.sub(r'common/([a-zA-Z0-9]){3}/', 'common/'+team_id+'/', path)
     path = re.sub(r'u0([a-zA-Z0-9]){3}p', 'u0'+team_id+'p', path)
     path = re.sub(r'u0([a-zA-Z0-9]){3}g', 'u0'+team_id+'g', path)
 

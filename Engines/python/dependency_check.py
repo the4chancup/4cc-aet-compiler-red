bd8817d2c5fc8e7dc5691fa5bca26dd2d6c71059
Shakes4cc <jappopiano@gmail.com>
2024-05-18 23:27:09 +0200

    Add commentedconfigparser to dependencies list
    This requires upgrading the list to support modules whose name is
    different than the name used to install them from pip
    Also improve messages

--- Engines/python/dependency_check.py
+++ Engines/python/dependency_check.py
@@ -6,34 +6,37 @@ import importlib.util
 # Check if the dependencies are installed
 def dependency_check_on_import():
 
-    # Prepare a list of dependencies
+    # Prepare a list of dependencies as dictionaries with name and name_pip
     dependencies = [
-        "py7zr",
-        "requests",
-        "traceback_with_variables",
+        {"name": "py7zr",                    "name_pip": "py7zr"},
+        {"name": "requests",                 "name_pip": "requests"},
+        {"name": "commentedconfigparser",    "name_pip": "commented-configparser"},
+        {"name": "traceback_with_variables", "name_pip": "traceback_with_variables"},
     ]
 
     dependencies_missing = []
 
     for dependency in dependencies:
-        if importlib.util.find_spec(dependency) is None:
+        if importlib.util.find_spec(dependency["name"]) is None:
             # Add any missing dependencies to the list
-            dependencies_missing.append(dependency)
+            dependencies_missing.append(dependency["name_pip"])
 
     if dependencies_missing:
 
-        print("- The following new dependencies were not found:")
+        print("-")
+        print("- The following dependencies were not found:")
         # List the missing dependencies, one per line
         for dependency in dependencies_missing:
             print(f"- \"{dependency}\"")
-        print("-")
 
+        print("-")
         print("- They will be installed now (or you can close the program now and install them manually).")
         print("- Once the installation is complete, the program will be closed automatically.")
         print("- Please run it again afterwards.")
         print("-")
-        input("Press Enter to install...")
+        input("Press Enter to install... ")
 
+        print("-")
         print("- Installing...")
 
         # Install the dependencies (closes the program automatically after the installation)
@@ -41,6 +44,6 @@ def dependency_check_on_import():
         runpy.run_module("pip", run_name="__main__")
 
 
-# Run the module when imported
+# Run the check when imported
 if __name__.endswith("dependency_check"):
     dependency_check_on_import()

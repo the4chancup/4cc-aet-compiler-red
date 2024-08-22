386190090490cc0f9e8075bfbb9d70376a46e85b
Shakes4cc <jappopiano@gmail.com>
2024-08-23 01:17:31 +0200

    Invert slashes on file paths

--- Engines/python/lib/utils/FILE_INFO.py
+++ Engines/python/lib/utils/FILE_INFO.py
@@ -1,33 +1,33 @@
 # Folder paths
-EXPORTS_TO_ADD_PATH          = "./exports_to_add"
-EXTRACTED_PATH               = "./extracted"
-PATCHES_CONTENTS_PATH        = "./patches_contents"
-PATCHES_OUTPUT_PATH          = "./patches_output"
+EXPORTS_TO_ADD_PATH          = ".\\exports_to_add"
+EXTRACTED_PATH               = ".\\extracted"
+PATCHES_CONTENTS_PATH        = ".\\patches_contents"
+PATCHES_OUTPUT_PATH          = ".\\patches_output"
 
 # Settings file
-SETTINGS_PATH                = "./settings.ini"
+SETTINGS_PATH                = ".\\settings.ini"
 
 # Team files
-TEAMS_LIST_PATH              = "./teams_list.txt"
-TEAMNOTES_PATH               = "./teamnotes.txt"
+TEAMS_LIST_PATH              = ".\\teams_list.txt"
+TEAMNOTES_PATH               = ".\\teamnotes.txt"
 
 # Logs
-SUGGESTIONS_LOG_PATH         = "./suggestions.log"
-ISSUES_LOG_PATH              = "./issues.log"
-CRASH_LOG_PATH               = "./crash.log"
+SUGGESTIONS_LOG_PATH         = ".\\suggestions.log"
+ISSUES_LOG_PATH              = ".\\issues.log"
+CRASH_LOG_PATH               = ".\\crash.log"
 
 # Run batch
-RUN_BATCH_PATH               = "./Engines/compiler_run.bat"
+RUN_BATCH_PATH               = ".\\Engines\\compiler_run.bat"
 
 # Update files
-CHECK_LAST_PATH              = "./Engines/update_check_last.txt"
-SKIP_LAST_PATH               = "./Engines/update_skip_last.txt"
+CHECK_LAST_PATH              = ".\\Engines\\update_check_last.txt"
+SKIP_LAST_PATH               = ".\\Engines\\update_skip_last.txt"
 
 # Admin tools
-ADMIN_WARNED_PATH            = "./Engines/admin_warned.txt"
+ADMIN_WARNED_PATH            = ".\\Engines\\admin_warned.txt"
 
 # Template files
-TEMPLATE_FOLDER_PATH         = "./Engines/templates"
+TEMPLATE_FOLDER_PATH         = ".\\Engines\\templates"
 DUMMY_MTL_NAME               = "dummy.mtl"
 DUMMY_MODEL_NAME             = "oral_dummy_win32.model"
 GENERIC_FPKD_NAME            = "generic.fpkd"
@@ -35,11 +35,11 @@ KIT_MASK_NAME                = "kit_mask.dds"
 FACE_DIFF_BIN_NAME           = "face_diff.bin"
 
 # Settings tools
-SETTINGS_DEFAULT_PATH        = "./Engines/templates/settings_default.ini"
-SETTINGS_TRANSFER_TABLE_PATH = "./Engines/templates/settings_transfer_table.txt"
+SETTINGS_DEFAULT_PATH        = ".\\Engines\\templates\\settings_default.ini"
+SETTINGS_TRANSFER_TABLE_PATH = ".\\Engines\\templates\\settings_transfer_table.txt"
 
 # Bin files
-BIN_FOLDER_PATH             = "./Engines/bins"
+BIN_FOLDER_PATH             = ".\\Engines\\bins"
 TEAMCOLOR_BIN_NAME           = "TeamColor.bin"
 UNICOLOR_BIN_NAME            = "UniColor.bin"
 UNIPARAM_NAME                = "UniformParameter.bin"
@@ -47,4 +47,4 @@ UNIPARAM_18_NAME             = "UniformParameter18.bin"
 UNIPARAM_19_NAME             = "UniformParameter19.bin"
 
 # DirectXTex
-TEXCONV_PATH                 = "./Engines/directxtex/texconv.exe"
+TEXCONV_PATH                 = ".\\Engines\\directxtex\\texconv.exe"

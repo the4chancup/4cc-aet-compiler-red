720aa1e16045561a51a86462b8a98060a4f7cc32
Shakes4cc <jappopiano@gmail.com>
2024-08-22 23:50:22 +0200

    Convert variable colorize to constant

--- Engines/python/lib/utils/COLORS.py
+++ Engines/python/lib/utils/COLORS.py
@@ -8,32 +8,32 @@ if sys.platform == "win32":
     win_version = sys.getwindowsversion().major
 
     # Check if the NO_COLOR environment variable is set
-    colorize = "NO_COLOR" not in os.environ and win_version >= 10
+    COLORIZE = "NO_COLOR" not in os.environ and win_version >= 10
 
-    if colorize:
+    if COLORIZE:
         os.system("color")
 
 else:
     # Check if the NO_COLOR environment variable is set
-    colorize = "NO_COLOR" not in os.environ
+    COLORIZE = "NO_COLOR" not in os.environ
 
-BLACK          = '\033[30m' if colorize else ''
+BLACK          = '\033[30m' if COLORIZE else ''
 
-DARK_RED       = '\033[31m' if colorize else ''
-DARK_GREEN     = '\033[32m' if colorize else ''
-DARK_YELLOW    = '\033[33m' if colorize else ''
-DARK_BLUE      = '\033[34m' if colorize else ''
-DARK_MAGENTA   = '\033[35m' if colorize else ''
-DARK_CYAN      = '\033[36m' if colorize else ''
-DARK_WHITE     = '\033[37m' if colorize else ''
+DARK_RED       = '\033[31m' if COLORIZE else ''
+DARK_GREEN     = '\033[32m' if COLORIZE else ''
+DARK_YELLOW    = '\033[33m' if COLORIZE else ''
+DARK_BLUE      = '\033[34m' if COLORIZE else ''
+DARK_MAGENTA   = '\033[35m' if COLORIZE else ''
+DARK_CYAN      = '\033[36m' if COLORIZE else ''
+DARK_WHITE     = '\033[37m' if COLORIZE else ''
 
-BRIGHT_BLACK   = '\033[90m' if colorize else ''
-BRIGHT_RED     = '\033[91m' if colorize else ''
-BRIGHT_GREEN   = '\033[92m' if colorize else ''
-BRIGHT_YELLOW  = '\033[93m' if colorize else ''
-BRIGHT_BLUE    = '\033[94m' if colorize else ''
-BRIGHT_MAGENTA = '\033[95m' if colorize else ''
-BRIGHT_CYAN    = '\033[96m' if colorize else ''
-WHITE          = '\033[97m' if colorize else ''
+BRIGHT_BLACK   = '\033[90m' if COLORIZE else ''
+BRIGHT_RED     = '\033[91m' if COLORIZE else ''
+BRIGHT_GREEN   = '\033[92m' if COLORIZE else ''
+BRIGHT_YELLOW  = '\033[93m' if COLORIZE else ''
+BRIGHT_BLUE    = '\033[94m' if COLORIZE else ''
+BRIGHT_MAGENTA = '\033[95m' if COLORIZE else ''
+BRIGHT_CYAN    = '\033[96m' if COLORIZE else ''
+WHITE          = '\033[97m' if COLORIZE else ''
 
-RESET          = '\033[0m' if colorize else ''
+RESET          = '\033[0m' if COLORIZE else ''

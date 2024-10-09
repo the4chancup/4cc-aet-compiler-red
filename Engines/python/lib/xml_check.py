the4chancup/4cc-aet-compiler-red

commit b37c98eae584173fe6dd724728de5b6222058d2c

Clean listed_file_check some more

Engines/python/lib/xml_check.py
@@ -67,8 +67,6 @@ def listed_file_check(xml_path, xml_name, xml_folder_name, listed_file_path, lis
     # Get the extension of the xml file
     xml_extension = os.path.splitext(xml_path)[1][1:].upper()
 
-    error = False
-
     if not listed_file_path:
         logging.error( "-")
         logging.error(f"- ERROR - Missing {listed_file_type.lower()} path")
@@ -99,7 +97,7 @@ def listed_file_check(xml_path, xml_name, xml_folder_name, listed_file_path, lis
 
         error_file_missing = not file_exists(file_path)
 
-    # Check if the file path points to the uniform common folder and the file exists in the Common folder of the export
+    # Check if the file path points to the uniform common folder
     elif listed_file_path.startswith('model/character/uniform/common/'):
 
         # If the PES version is 16 and the file is a model file, throw an error
@@ -120,20 +118,21 @@ def listed_file_check(xml_path, xml_name, xml_folder_name, listed_file_path, lis
         # Replace * in the path with "win32"
         file_path = file_path.replace('*', 'win32')
 
-        if not file_exists(file_path):
+        if file_exists(file_path):
+            return False
 
-            # Search for the file in every midcup cpk and the faces cpk in the download folder
-            listed_file_path_real = "common/character1/" + path_id_change(listed_file_path, team_id).replace('p0', 'p1')
-            file_info_list = [
-                {'source_path': listed_file_path_real},
-            ]
-            cpk_names_list = ['midcup', 'uniform', 'faces']
+        # Search for the file in every midcup cpk and the faces cpk in the download folder
+        listed_file_path_real = "common/character1/" + path_id_change(listed_file_path, team_id).replace('p0', 'p1')
+        file_info_list = [
+            {'source_path': listed_file_path_real},
+        ]
+        cpk_names_list = ['midcup', 'uniform', 'faces']
 
-            error_file_missing = not files_fetch_from_cpks(file_info_list, cpk_names_list, fetch=False)
+        error_file_missing = not files_fetch_from_cpks(file_info_list, cpk_names_list, fetch=False)
 
     # Check if the file path points to the face common folder
     elif listed_file_path.startswith('model/character/face/common/'):
-        pass
+        return False
 
     # If the file path is not a relative path nor points to the common folders, it is unusable
     else:
@@ -141,36 +140,35 @@ def listed_file_check(xml_path, xml_name, xml_folder_name, listed_file_path, lis
 
         error_file_missing = True
 
-    if error_file_missing:
+    if not error_file_missing:
+        return False
 
-        if not file_path_short:
-            # Remove the extracted folder path and / from the path
-            extracted_path_length = len(EXTRACTED_PATH)
-            file_path_short = file_path[(extracted_path_length+1):]
+    if not file_path_short:
+        # Remove the extracted folder path and / from the path
+        extracted_path_length = len(EXTRACTED_PATH)
+        file_path_short = file_path[(extracted_path_length+1):]
 
-        ##TODO: Make error-only and merge once the templates have been updated
-        type_string_raw = f"{listed_file_type} path:"
-        type_string = type_string_raw + " " * (16 - len(type_string_raw))
-        if listed_file_type == "Texture":
-            logging.warning( "-")
-            logging.warning(f"- Warning - {listed_file_type} file does not exist in the path indicated")
-            logging.warning(f"- Folder:         {xml_folder_name}")
-            logging.warning(f"- {xml_extension} name:       {xml_name}")
-            logging.warning(f"- Material:       {material_name}")
-            logging.warning(f"- Sampler:        {sampler_name}")
-            logging.warning(f"- {type_string}{listed_file_path}")
-            logging.warning(f"- Full path:      {file_path_short}")
-        else:
-            logging.error( "-")
-            logging.error(f"- ERROR - {listed_file_type} file does not exist in the path indicated")
-            logging.error(f"- Folder:         {xml_folder_name}")
-            logging.error(f"- {xml_extension} name:       {xml_name}")
-            logging.error(f"- {type_string}{listed_file_path}")
-            logging.error(f"- Full path:      {file_path_short}")
+    ##TODO: Make error-only and merge once the templates have been updated
+    type_string_raw = f"{listed_file_type} path:"
+    type_string = type_string_raw + " " * (16 - len(type_string_raw))
+    if listed_file_type == "Texture":
+        logging.warning( "-")
+        logging.warning(f"- Warning - {listed_file_type} file does not exist in the path indicated")
+        logging.warning(f"- Folder:         {xml_folder_name}")
+        logging.warning(f"- {xml_extension} name:       {xml_name}")
+        logging.warning(f"- Material:       {material_name}")
+        logging.warning(f"- Sampler:        {sampler_name}")
+        logging.warning(f"- {type_string}{listed_file_path}")
+        logging.warning(f"- Full path:      {file_path_short}")
+    else:
+        logging.error( "-")
+        logging.error(f"- ERROR - {listed_file_type} file does not exist in the path indicated")
+        logging.error(f"- Folder:         {xml_folder_name}")
+        logging.error(f"- {xml_extension} name:       {xml_name}")
+        logging.error(f"- {type_string}{listed_file_path}")
+        logging.error(f"- Full path:      {file_path_short}")
 
-            error = True
-
-    return error
+    return True
 
 
 def face_diff_xml_check(xml_path):

commit 9258d8ec74de090ceb34b2e688b1e1710ff9cb93

Fix regression with warning returning error
Caused by commit b37c98eae584173fe6dd724728de5b6222058d2c

Engines/python/lib/xml_check.py
@@ -173,13 +173,15 @@ def listed_file_check(xml_path, xml_name, xml_folder_name, listed_file_path, lis
         logging.warning(f"- Sampler:        {sampler_name}")
         logging.warning(f"- {type_string}{listed_file_path}")
         logging.warning(f"- Full path:      {file_path_short}")
-    else:
-        logging.error( "-")
-        logging.error(f"- ERROR - {listed_file_type} file does not exist in the path indicated")
-        logging.error(f"- Folder:         {xml_folder_name}")
-        logging.error(f"- {xml_extension} name:       {xml_name}")
-        logging.error(f"- {type_string}{listed_file_path}")
-        logging.error(f"- Full path:      {file_path_short}")
+
+        return False
+
+    logging.error( "-")
+    logging.error(f"- ERROR - {listed_file_type} file does not exist in the path indicated")
+    logging.error(f"- Folder:         {xml_folder_name}")
+    logging.error(f"- {xml_extension} name:       {xml_name}")
+    logging.error(f"- {type_string}{listed_file_path}")
+    logging.error(f"- Full path:      {file_path_short}")
 
     return True
 
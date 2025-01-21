97a6e67d34c476c3749b6952927825d141df6ccf
Shakes4cc <jappopiano@gmail.com>
2025-01-21 22:37:04 +0100

    Use os.path.join on FILE_INFO to prevent issues

--- Engines/python/lib/utils/FILE_INFO.py
+++ Engines/python/lib/utils/FILE_INFO.py
@@ -1,33 +1,35 @@
+import os
+
 # Folder paths
-EXPORTS_TO_ADD_PATH          = "./exports_to_add"
-EXTRACTED_PATH               = "./extracted"
-PATCHES_CONTENTS_PATH        = "./patches_contents"
-PATCHES_OUTPUT_PATH          = "./patches_output"
+EXPORTS_TO_ADD_PATH          = "exports_to_add"
+EXTRACTED_PATH               = "extracted"
+PATCHES_CONTENTS_PATH        = "patches_contents"
+PATCHES_OUTPUT_PATH          = "patches_output"
 
 # Settings file
-SETTINGS_PATH                = "./settings.ini"
+SETTINGS_PATH                = os.path.join("settings.ini")
 
 # Team files
-TEAMS_LIST_PATH              = "./teams_list.txt"
-TEAMNOTES_PATH               = "./teamnotes.txt"
+TEAMS_LIST_PATH              = "teams_list.txt"
+TEAMNOTES_PATH               = "teamnotes.txt"
 
 # Logs
-SUGGESTIONS_LOG_PATH         = "./suggestions.log"
-ISSUES_LOG_PATH              = "./issues.log"
-CRASH_LOG_PATH               = "./crash.log"
+SUGGESTIONS_LOG_PATH         = "suggestions.log"
+ISSUES_LOG_PATH              = "issues.log"
+CRASH_LOG_PATH               = "crash.log"
 
 # Run batch
-RUN_BATCH_PATH               = "./Engines/compiler_run.bat"
+RUN_BATCH_PATH               = os.path.join("Engines", "compiler_run.bat")
 
 # Update files
-CHECK_LAST_PATH              = "./Engines/update_check_last.txt"
-SKIP_LAST_PATH               = "./Engines/update_skip_last.txt"
+CHECK_LAST_PATH              = os.path.join("Engines", "update_check_last.txt")
+SKIP_LAST_PATH               = os.path.join("Engines", "update_skip_last.txt")
 
 # Admin tools
-ADMIN_WARNED_PATH            = "./Engines/admin_warned.txt"
+ADMIN_WARNED_PATH            = os.path.join("Engines", "admin_warned.txt")
 
 # Template files
-TEMPLATE_FOLDER_PATH         = "./Engines/templates"
+TEMPLATE_FOLDER_PATH         = os.path.join("Engines", "templates")
 DUMMY_MTL_NAME               = "dummy.mtl"
 DUMMY_MODEL_NAME             = "oral_dummy_win32.model"
 GENERIC_FPKD_NAME            = "generic.fpkd"
@@ -35,11 +37,11 @@ KIT_MASK_NAME                = "kit_mask.dds"
 FACE_DIFF_BIN_NAME           = "face_diff.bin"
 
 # Settings tools
-SETTINGS_DEFAULT_PATH        = "./Engines/templates/settings_default.ini"
-SETTINGS_TRANSFER_TABLE_PATH = "./Engines/templates/settings_transfer_table.txt"
+SETTINGS_DEFAULT_PATH        = os.path.join("Engines", "templates", "settings_default.ini")
+SETTINGS_TRANSFER_TABLE_PATH = os.path.join("Engines", "templates", "settings_transfer_table.txt")
 
 # Bin files
-BIN_FOLDER_PATH             = "./Engines/bins"
+BIN_FOLDER_PATH              = os.path.join("Engines", "bins")
 TEAMCOLOR_BIN_NAME           = "TeamColor.bin"
 UNICOLOR_BIN_NAME            = "UniColor.bin"
 UNIPARAM_NAME                = "UniformParameter.bin"
@@ -47,7 +49,7 @@ UNIPARAM_18_NAME             = "UniformParameter18.bin"
 UNIPARAM_19_NAME             = "UniformParameter19.bin"
 
 # DirectXTex
-TEXCONV_PATH                 = "./Engines/directxtex/texconv.exe"
+TEXCONV_PATH                 = os.path.join("Engines", "directxtex", "texconv.exe")
 
 # Compiler paths
-PATCHES_CONTENTS_REFS_PATH  = "./Engines/templates/patches_contents_refs"
+PATCHES_CONTENTS_REFS_PATH   = os.path.join("Engines", "templates", "patches_contents_refs")

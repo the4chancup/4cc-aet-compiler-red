9826383b33fe75a6c86ee84bf1980d6d83cfa8a9
Shakes4cc <jappopiano@gmail.com>
2025-09-05 23:03:38 +0200

    Add coloring to the fatal errors on compiler_run

--- Engines/compiler_run.bat
+++ Engines/compiler_run.bat
@@ -5,13 +5,27 @@ REM - Set the working folder
 cd /D "%~dp0\.."
 
 
+REM - Set the ESC character and colors for text coloring
+for /f "delims=" %%E in ('forfiles /p "%~dp0." /m "%~nx0" /c "cmd /c echo(0x1B"') do (
+    set "ESC=%%E"
+)
+if not defined NO_COLOR (
+    set "DARK_RED=%ESC%[31m"
+    set "BRIGHT_MAGENTA=%ESC%[95m"
+    set "RESET=%ESC%[0m"
+) else (
+    set "DARK_RED="
+    set "BRIGHT_MAGENTA="
+    set "RESET="
+)
+
 REM - Check if python is installed and was added to the PATH
 if exist ".\Engines\python_check.bat" (
     call .\Engines\python_check.bat
     REM - This script sets the version on python_version
 ) else (
     echo -
-    echo - FATAL ERROR - Missing vital file
+    echo - %DARK_RED%FATAL ERROR%RESET% - Missing vital file
     echo - The file python_check.bat was not found in the Engines folder
     echo -
     echo - Please grab a clean compiler folder
@@ -30,7 +44,7 @@ if exist ".\Engines\compiler_main.py" (
     call py -%python_version% .\Engines\compiler_main.py %running_type_num%
 ) else (
     echo -
-    echo - FATAL ERROR - Missing vital file
+    echo - %DARK_RED%FATAL ERROR%RESET% - Missing vital file
     echo - The file compiler_main.py was not found in the Engines folder
     echo -
     echo - Please grab a clean compiler folder
@@ -43,18 +57,6 @@ set crashed=%ERRORLEVEL%
 REM - Run the log cleaner
 call py -%python_version% .\Engines\log_username_clean.py
 
-
-REM - Set the ESC character and colors for text coloring
-for /f "delims=" %%E in ('forfiles /p "%~dp0." /m "%~nx0" /c "cmd /c echo(0x1B"') do (
-    set "ESC=%%E"
-)
-if not defined NO_COLOR (
-    set "BRIGHT_MAGENTA=%ESC%[95m"
-    set "RESET=%ESC%[0m"
-) else (
-    set "BRIGHT_MAGENTA="
-    set "RESET="
-)
 
 REM - If the compiler returned an error
 if %crashed% GEQ 1 (
